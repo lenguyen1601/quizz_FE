49ae024aadd5accab04f98644033a325045dfc59
lenguyen1601 <110454396+lenguyen1601@users.noreply.github.com>
2023-01-28 17:35:40 +0700

    Creating Add Questions Form in QUIZ | Admin Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,8 @@ import { AddQuizComponent } from './pages/admin/add-quiz/add-quiz.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSelectModule } from '@angular/material/select';
 import { UpdateQuizComponent } from './pages/admin/update-quiz/update-quiz.component';
+import { ViewQuizQuestionsComponent } from './pages/admin/view-quiz-questions/view-quiz-questions.component';
+import { AddQuestionComponent } from './pages/admin/add-question/add-question.component';
 
 @NgModule({
   declarations: [
@@ -52,6 +54,10 @@ import { UpdateQuizComponent } from './pages/admin/update-quiz/update-quiz.compo
     ViewQuizzesComponent,
     AddQuizComponent,
     UpdateQuizComponent,
+
+    ViewQuizQuestionsComponent,
+
+    AddQuestionComponent,
   ],
 
   imports: [
---
+++ src/app/pages/admin/view-quiz-questions/view-quiz-questions.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QuestionService } from 'src/app/services/question.service';
+
+@Component({
+  selector: 'app-view-quiz-questions',
+  templateUrl: './view-quiz-questions.component.html',
+  styleUrls: ['./view-quiz-questions.component.css'],
+})
+export class ViewQuizQuestionsComponent implements OnInit {
+  qId;
+  qTitle;
+  questions = [
+    {
+      content: 'dcsdc',
+      option1: 'a',
+      option2: 'b',
+      option3: 'c',
+      option4: 'd',
+      answer: 'a',
+    },
+  ];
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _question: QuestionService
+  ) {}
+
+  ngOnInit(): void {
+    this.qId = this._route.snapshot.params.qid;
+    this.qTitle = this._route.snapshot.params.title;
+    this._question.getQuestionsOfQuiz(this.qId).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.questions = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
